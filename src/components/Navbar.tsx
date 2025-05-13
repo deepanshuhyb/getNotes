fd66fdef4e4609e8e553d71367cd2b699cf56f79
deepanshuhyb <deepanshuhyb@gmail.com>
2025-05-13 23:52:00 +0530

    Minor UI update in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
     const handleClick = () => {
         alert('Search clicked')
     }
-    const classForNavbar = isNavbarBigClicked ? 'lg:w-[40rem]' : 'lg:w-96'
+    const classForNavbar = isNavbarBigClicked ? 'lg:w-[40rem] md:w-96' : 'lg:w-96'
 
     return <>
         <div className="h-16 md:h-20 w-full bg-black border-b text-white flex align-center justify-between md:px-12 py-2 px-4 sticky top-0 z-10">
@@ -18,7 +18,7 @@ export default function Navbar() {
             <form action="" className='my-auto relative'>
                 <input
                     type="text"
-                    className={`my-auto flex h-9 md:h-12 w-44 rounded-md border border-input bg-transparent px-3 py-1 md:text-base text-sm transition-all duration-300 ease-in-out ${classForNavbar}`}
+                    className={`my-auto flex h-9 md:h-12 w-44 md:w-72 rounded-md border border-input bg-transparent px-3 py-1 md:text-base text-sm transition-all duration-300 ease-in-out ${classForNavbar}`}
                     value={value}
                     onChange={e => setValue(e.target.value)}
                     onClick={() => setIsNavbarBigClicked(true)}
